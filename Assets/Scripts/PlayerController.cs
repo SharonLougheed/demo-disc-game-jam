ddbc750dacb7de220f22667695a2affe75cbca9c
Benjamin Adams <42784077+atomsand1101its@users.noreply.github.com>
2019-03-29 21:34:36 -0500

    Made cameras horizontal split & added controllers

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,24 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public int PlayerNumber = 1;
+    public int ControllerNumber = 1;
     public float RotationSpeed = 100f;
     public float MovementSpeed = 8f;
 
 
     private void Update()
     {
-        if (Input.GetAxis("Strafe" + PlayerNumber) > 0)
+        if (Input.GetAxis("Strafe" + ControllerNumber) > 0)
         {
-            float strafeMovementAmount = Input.GetAxis("Horizontal" + PlayerNumber) * MovementSpeed * Time.deltaTime;
+            float strafeMovementAmount = Input.GetAxis("Horizontal" + ControllerNumber) * MovementSpeed * Time.deltaTime;
             gameObject.transform.Translate(new Vector3(strafeMovementAmount, 0f, 0f));
         }
         else
         {
-            float rotationAmount = Input.GetAxis("Horizontal" + PlayerNumber) * RotationSpeed * Time.deltaTime;
+            float rotationAmount = Input.GetAxis("Horizontal" + ControllerNumber) * RotationSpeed * Time.deltaTime;
             gameObject.transform.Rotate(new Vector3(0f, rotationAmount));
         }
 
-        float movementAmount = Input.GetAxis("Vertical" + PlayerNumber) * MovementSpeed * Time.deltaTime;
+        float movementAmount = Input.GetAxis("Vertical" + ControllerNumber) * MovementSpeed * Time.deltaTime;
         gameObject.transform.Translate(new Vector3(0f, 0f, movementAmount));
     }
 }
