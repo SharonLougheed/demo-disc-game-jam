37ae8bfc307fd4ab4686140b007ad92c75a17350
Benjamin Adams <42784077+atomsand1101its@users.noreply.github.com>
2019-03-31 21:02:10 -0500

    Filled in folder structure and played w/ win/loss

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public bool isGameOver;
+
+    private void Start()
+    {
+        isGameOver = false;
+    }
+
+    private void Update()
+    {
+        CheckForWinner();
+    }
+
+    private void CheckForWinner()
+    {
+        var players = FindObjectsOfType<Player>();
+        var livingPlayers = new List<Player>();
+        foreach (var player in players)
+        {
+            if (player.IsAlive)
+            {
+                livingPlayers.Add(player);
+            }
+        }
+
+        if (livingPlayers.Count <= 1)
+        {
+            GameOver(livingPlayers[0]);
+        }
+    }
+
+    private void GameOver(Player lastPlayer)
+    {
+        isGameOver = true;
+        Debug.Log("Gameover, man!");
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public FloatVariable Health;
+    public bool IsAlive;
+
+    public void Kill()
+    {
+        IsAlive = false;
+    }
+}
