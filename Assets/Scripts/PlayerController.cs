99243c5b7b488373a332d3efcdd886f32a107a09
Benjamin Adams <42784077+atomsand1101its@users.noreply.github.com>
2019-04-01 21:59:42 -0500

    Change settings to ScriptableObjects
    Change the player speeds to use ScriptableObjects for movement and
    rotation. Add 4th player to demo but deactivat the controller.
    
    - Player one uses WADS + left shift
    - player two uses arrow keys + space
    - player three is set up to use a controller (I know Xbox controller
    works and A is strafe)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,25 +5,50 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public int ControllerNumber = 1;
-    public float RotationSpeed = 100f;
-    public float MovementSpeed = 8f;
+
+    // Rotation Speed at 100f was the original setting
+    // Movement Speed at 8f was the original setting
+    public ControlSettings Settings;
+
+
     public float SpeedUpPercent = 0f;
 
 
     private void Update()
     {
+        CheckTurnStrafe();
+        CheckForwardBack();
+    }
+
+    private void CheckForwardBack()
+    {
+        float movementAmount = Input.GetAxis("Vertical" + ControllerNumber)
+            * Settings.MovementSpeed
+            * Time.deltaTime
+            * (1 + (SpeedUpPercent / 100));
+
+        gameObject.transform.Translate(new Vector3(0f, 0f, movementAmount));
+    }
+
+    private void CheckTurnStrafe()
+    {
         if (Input.GetAxis("Strafe" + ControllerNumber) > 0)
         {
-            float strafeMovementAmount = Input.GetAxis("Horizontal" + ControllerNumber) * MovementSpeed * Time.deltaTime * (1 + (SpeedUpPercent / 100));
+            float strafeMovementAmount = Input.GetAxis("Horizontal" + ControllerNumber)
+                * Settings.MovementSpeed
+                * Time.deltaTime
+                * (1 + (SpeedUpPercent / 100));
+
             gameObject.transform.Translate(new Vector3(strafeMovementAmount, 0f, 0f));
         }
         else
         {
-            float rotationAmount = Input.GetAxis("Horizontal" + ControllerNumber) * RotationSpeed * Time.deltaTime * (1 + (SpeedUpPercent / 100));
+            float rotationAmount = Input.GetAxis("Horizontal" + ControllerNumber)
+                * Settings.RotationSpeed
+                * Time.deltaTime
+                * (1 + (SpeedUpPercent / 100));
+
             gameObject.transform.Rotate(new Vector3(0f, rotationAmount));
         }
-
-        float movementAmount = Input.GetAxis("Vertical" + ControllerNumber) * MovementSpeed * Time.deltaTime * (1 + (SpeedUpPercent / 100));
-        gameObject.transform.Translate(new Vector3(0f, 0f, movementAmount));
     }
 }
