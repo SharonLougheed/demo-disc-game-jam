391688fdb33e53e23206365d322e401ab2d06e96
Benjamin Adams <42784077+atomsand1101its@users.noreply.github.com>
2019-04-01 19:45:42 -0500

    Added Game Over check and Health/Death

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public FloatVariable Health;
+    public int PlayerNumber = 1;
+    public int Health;
+    public PlayerDefaults defaults;
     public bool IsAlive;
+
+    public void Awake()
+    {
+        Health = defaults.StartHealth;
+    }
+
+    private void Update()
+    {
+        // Debug damage, DELETE ME
+        if (Input.GetKeyDown(KeyCode.Q) && PlayerNumber == 2)
+        {
+            TakeDamage(15);
+        }
+    }
+
+    public void GiveHeath(int giveAmount)
+    {
+        Health += giveAmount;
+        if (Health > defaults.MaxHealth)
+        {
+            Health = defaults.MaxHealth;
+        }
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        Health -= damageAmount;
+        if (Health <= 0)
+        {
+            Kill();
+        }
+    }
 
     public void Kill()
     {
+        // Zero health here in case of insta-death
+        Health = 0;
         IsAlive = false;
+
+        // Destroy for now : We will want to leave the corpses
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int PlayerNumber = 1;
     public int ControllerNumber = 1;
     public float RotationSpeed = 100f;
     public float MovementSpeed = 8f;
+    public float SpeedUpPercent = 0f;
 
 
     private void Update()
     {
         if (Input.GetAxis("Strafe" + ControllerNumber) > 0)
         {
-            float strafeMovementAmount = Input.GetAxis("Horizontal" + ControllerNumber) * MovementSpeed * Time.deltaTime;
+            float strafeMovementAmount = Input.GetAxis("Horizontal" + ControllerNumber) * MovementSpeed * Time.deltaTime * (1 + (SpeedUpPercent / 100));
             gameObject.transform.Translate(new Vector3(strafeMovementAmount, 0f, 0f));
         }
         else
         {
-            float rotationAmount = Input.GetAxis("Horizontal" + ControllerNumber) * RotationSpeed * Time.deltaTime;
+            float rotationAmount = Input.GetAxis("Horizontal" + ControllerNumber) * RotationSpeed * Time.deltaTime * (1 + (SpeedUpPercent / 100));
             gameObject.transform.Rotate(new Vector3(0f, rotationAmount));
         }
 
-        float movementAmount = Input.GetAxis("Vertical" + ControllerNumber) * MovementSpeed * Time.deltaTime;
+        float movementAmount = Input.GetAxis("Vertical" + ControllerNumber) * MovementSpeed * Time.deltaTime * (1 + (SpeedUpPercent / 100));
         gameObject.transform.Translate(new Vector3(0f, 0f, movementAmount));
     }
 }
