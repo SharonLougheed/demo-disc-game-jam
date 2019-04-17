440eacb5f0901911fe487b216aa5ae1544d39e54
shaeboy <38510696+shaeboy@users.noreply.github.com>
2019-04-17 16:44:51 -0500

    MusicOffset Script
    Wrote a simple coroutine to offset the start of the level theme and fade in. Needs to be added to the LevelManager game object.

---
+++ Assets/Scripts/MusicOffset.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicOffset : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(FunkyJamShuffle());
+    }
+
+    IEnumerator FunkyJamShuffle()
+    {
+        var levelTheme = GetComponent<AudioSource>();
+        var volumeIncrement = levelTheme.volume / 20.0f;
+
+        levelTheme.time = Random.Range(0.0f, 120.0f);
+
+        for(int k = 0; k <= 20; k++)
+        {
+            levelTheme.volume = (float)k * volumeIncrement;
+
+            yield return new WaitForSeconds(.06f);
+        }
+
+        yield return null;
+    }
+}
