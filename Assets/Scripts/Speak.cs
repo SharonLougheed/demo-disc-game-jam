ae5738e13179ccf82511cf0b5b0548ac2555c972
shaeboy <38510696+shaeboy@users.noreply.github.com>
2019-04-27 17:04:44 -0500

    Bark Crates
    Added environmental bark sounds.

---
+++ Assets/Scripts/Speak.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speak : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine(MakeSomeNoise());
+    }
+
+    IEnumerator MakeSomeNoise()
+    {
+        var barks = GetComponents<AudioSource>();
+
+        var barkQuantity = barks.Length;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(2f, 6f));
+
+            if (Random.Range(0, 2) > 0 && barkQuantity > 0)
+            {
+                barks[Random.Range(0, barkQuantity)].Play();
+            }
+        }
+
+        yield return null;
+    }
+}
