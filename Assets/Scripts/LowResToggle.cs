dd3fa02624bcb1c017c355bb92bebd0fc7f66dfd
shaeboy <38510696+shaeboy@users.noreply.github.com>
2019-04-29 17:04:51 -0500

    Splash Tweaks
    Game over => splash

---
+++ Assets/Scripts/LowResToggle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LowResToggle : MonoBehaviour
+{
+    private void Awake()
+    {
+        if(GameObject.Find("GameResolution"))
+        {
+            GetComponent<Toggle>().isOn = GameObject.Find("GameResolution").GetComponent<GameResolution>().isLowRes;
+        }
+    }
+}
