3f05267c27e5a1228ac65984e035447863871840
shaeboy <38510696+shaeboy@users.noreply.github.com>
2019-04-30 17:55:12 -0500

    Cigar Edits
    Cigars throw more sparks on collision now, and make a fizzle sound, and also activate the Juke Box.

--- Assets/Scripts/Cigar.cs
+++ Assets/Scripts/Cigar.cs
@@ -10,35 +10,48 @@ public class Cigar : MonoBehaviour
 
     public GameObject concusser;
     public GameObject cigarImage;
+    public GameObject sparkler;
+
+    private bool isImmolating = false;
 
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        if (!isImmolating)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag.Equals("Player")
-                && !ThrowingPlayer.Equals(other.GetComponent<Player>()))
+        if (!other.gameObject.tag.Equals("Tables") && !isImmolating)
         {
-            Player player = other.gameObject.GetComponent<Player>();
-            player.TakeDamage(stats.CigarDamage);
-        }
+            if (other.gameObject.tag.Equals("Player")
+                    && !ThrowingPlayer.Equals(other.GetComponent<Player>()))
+            {
+                other.gameObject.GetComponent<Player>().TakeDamage(stats.CigarDamage);
+            }
 
-        if (!other.gameObject.tag.Equals("Tables"))
-        {
+            if (other.gameObject.tag.Equals("JUKE BOX"))
+            {
+                other.GetComponent<JukeBox>().SmashJuke();
+            }
+
             StartCoroutine(Immolate());
         }
     }
 
     IEnumerator Immolate()
     {
+        isImmolating = true;
+
         concusser.SetActive(true);
+        sparkler.SetActive(true);
         cigarImage.SetActive(false);
 
         yield return new WaitForSeconds(1.5f);
 
-        Destroy(this.gameObject);
+        Destroy(gameObject);
 
         yield return null;
     }
