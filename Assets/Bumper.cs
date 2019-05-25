85a0ab1ce87d8d640ae735193ba63c55783ee43d
hosehesquivel <49042246+hosehesquivel@users.noreply.github.com>
2019-05-25 05:40:04 -0500

    Logo Bumper
    logo bumper scene added to build settings.

---
+++ Assets/Bumper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    public Animator animator;
+	[SerializeField] string ToMainMenu;
+	
+	// Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	public void OnFadeComplete ()
+	{
+		SceneManager.LoadScene(ToMainMenu);
+	}
+}
